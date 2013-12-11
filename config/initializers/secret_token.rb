f54a0d767837e35aa8d89b17cad7046920ca53ff
Paul Anderson <anderson.paul.martin@gmail.com>
2013-12-11 17:17:44 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LoveMaybe::Application.config.secret_key_base = 'd508e68066e4dc8ffc473d0858ca61e90b00d57065c6a14a2a2aa86edc974baad0f838aa236179aea9e0beb802da51d2d61cf549d7881e9cbd6a6945f3618ee8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LoveMaybe::Application.config.session_store :cookie_store, key: '_love_maybe_session'
