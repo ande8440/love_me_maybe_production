521e3b2f4540ce099254f997993f57e68891c0f9
Paul Anderson <anderson.paul.martin@gmail.com>
2013-12-16 17:16:47 -0500

    added dating site dropdown

--- app/models/user_dating_username.rb
+++ app/models/user_dating_username.rb
@@ -1,3 +1,6 @@
 class UserDatingUsername < ActiveRecord::Base
 	belongs_to :user
+
+	validates :dating_site_website, :inclusion => {:in => ['OkCupid', 'Tinder', 'eHarmony', 'Match.com', 'PlentyofFish', 'Zoosk', 'Christian Mingle', 'DateHookup', 'HowAboutWe', 'Coffee Meets Bagel', 'BlackPeopleMeet', 'JDate', 'Badoo', 'Meetme', 'Skout', 'SeniorPeopleMeet']}, allow_blank: true
+
 end
